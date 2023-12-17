59e05255d4b57c7199d844b9ef75b772fc2c51ae
Mouhamedoune  Fall <MEDOUNEFALL126@GMAIL.COM>
2023-12-17 11:18:26 +0000

    Second exercise done

--- projet1.py
+++ projet1.py
@@ -14,42 +14,86 @@ def trouverUneCombinaison(number):
 
     return tableau
 
-#Going to the  second Exercise
-def users_bank_trie_croissant(tableau):
-    comptes = [{'nom': 'Diouf', 'prenom': 'Modou', 'epargne': 2500},
-               {'nom': 'Sene', 'prenom': 'Pathé', 'epargne': 5000},
-               {'nom': 'Ngom', 'prenom': 'Khadim', 'epargne': 10000},
-               {'nom': 'Ndiaye', 'prenom': 'Fatou', 'epargne': 1250},
-               {'nom': 'Mbengue', 'prenom': 'Alice', 'epargne': 2500},
-               {'nom': 'Déme', 'prenom': 'Maty'},
-               {'nom': 'Ngom', 'prenom': 'Satou', 'epargne': 4530},
-               {'nom': 'Thioune', 'prenom': 'Ameth', 'epargne': 2200}]
+#Going to do the  second Exercise
+
+#Part 1
+def users_bank_trie_croissant(comptes):
+    valid_accounts = [account for account in comptes if 'epargne' in account]
+
+    if valid_accounts:
+        min_account = valid_accounts[0]
+
+        for account in valid_accounts[1:]:
+            if 'epargne' in account and account['epargne'] < min_account.get('epargne'):
+                min_account = account
+
+    return "{" + min_account.get('nom') + "," + str(min_account.get('epargne')) + "}"
 
 
-    return comptes.sort
+#Part 2
+def users_bank_trie_decroissant(comptes):
+    valid_accounts = [account for account in comptes if 'epargne' in account]
 
+    if valid_accounts:
+        max_account = valid_accounts[0]
+
+        for account in valid_accounts[1:]:
+            if 'epargne' in account and account['epargne'] > max_account.get('epargne'):
+                max_account = account
+
+    return "{" + max_account.get('nom') + "," + str(max_account.get('epargne')) + "}"
+
+#Exercice 2 done
 
 if __name__ == '__main__':
-    #number = int(input("Veuillez entrer un nombre pour faire la combinaison du jeu de dés:"))
 
-    '''
-        determinons le nombre de combinaison 
-        pour faire le boucle
-    '''
-    comptes = [{'nom': 'Diouf', 'prenom': 'Modou', 'epargne': 2500},
-               {'nom': 'Sene', 'prenom': 'Pathé', 'epargne': 5000},
-               {'nom': 'Ngom', 'prenom': 'Khadim', 'epargne': 10000},
-               {'nom': 'Ndiaye', 'prenom': 'Fatou', 'epargne': 1250},
-               {'nom': 'Mbengue', 'prenom': 'Alice', 'epargne': 2500},
-               {'nom': 'Déme', 'prenom': 'Maty'},
-               {'nom': 'Ngom', 'prenom': 'Satou', 'epargne': 4530},
-               {'nom': 'Thioune', 'prenom': 'Ameth', 'epargne': 2200}]
+    comptes = [
+        {'nom': 'Diouf', 'prenom': 'Modou', 'epargne': 2500},
+        {'nom': 'Sene', 'prenom': 'Pathé', 'epargne': 5000},
+        {'nom': 'Ngom', 'prenom': 'Khadim', 'epargne': 10000},
+        {'nom': 'Ndiaye', 'prenom': 'Fatou', 'epargne': 1250},
+        {'nom': 'Mbengue', 'prenom': 'Alice', 'epargne': 2500},
+        {'nom': 'Déme', 'prenom': 'Maty'},
+        {'nom': 'Ngom', 'prenom': 'Satou', 'epargne': 4530},
+        {'nom': 'Thioune', 'prenom': 'Ameth', 'epargne': 2200}
+    ]
 
-    print(comptes[0]['epargne'])
+    # Test for the first exercise
 
-    #result = trouverUneCombinaison(number)
-    #print(result)
+    number = int(input("Entrer un nombre:"))
 
-    #print(tableau)
+    res = trouverUneCombinaison(number)
 
-    #print("Hello Mouhamedoune")
+    print(res)
+
+    # Test for the first exercise done sucessfully
+
+
+
+
+    #Test for the second exercise
+
+    """ PART 1 """
+    res1 = users_bank_trie_croissant(comptes)
+
+    print(res1)
+
+    """ PART 2 """
+    res2 = users_bank_trie_decroissant(comptes)
+
+    print(res2)
+
+    # Test for the second exercise done sucessfully
+
+
+
+
+    # Test for the third exercise
+
+    """ PART 1 """
+
+    """ PART 2 """
+    # Test for the third exercise done sucessfully
+
+
+
